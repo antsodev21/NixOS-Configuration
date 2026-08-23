e016e7a934661154ee48931ed24c1e7c164e2c3f
Antonio Martínez (Antsoftware) <antsoftware2107@proton.me>
2026-08-23 09:06:25 +0200

    Install OhMyBash and copy .bashrc
    Added installation of OhMyBash and copying of .bashrc.

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ cp .r config/* ~/.config
 # Ahora la config de NixOS
 sudo cp configuration.nix /etc/nixos/configuration.nix
 sudo nixos-rebuild switch
+
+# Instala OhMyBash y Copia mi .bashrc
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+cp .bashrc ~/
